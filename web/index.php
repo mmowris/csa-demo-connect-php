e602bbb46603d84c6b2f0dcba0b7cb1d1c2d9779
Matt Mowris <matt.mowris@gmail.com>
2016-08-22 11:32:22 -0700

    database access

--- web/index.php
+++ web/index.php
@@ -22,4 +22,13 @@ $app->get('/', function() use($app) {
   return $app['twig']->render('index.twig');
 });
 
+$dbopts = parse_url(getenv('DATABASE_URL'));
+$app->register(new Herrera\Pdo\PdoServiceProvider(),
+               array(
+                   'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"] . ';port=' . $dbopts["port"],
+                   'pdo.username' => $dbopts["user"],
+                   'pdo.password' => $dbopts["pass"]
+               )
+);
+
 $app->run();
