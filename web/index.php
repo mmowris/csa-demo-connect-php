04a73c3dcdc4ce930c9dc7cbe949839cde55c570
Matt Mowris <matt.mowris@gmail.com>
2016-08-22 11:38:47 -0700

    working DB

--- web/index.php
+++ web/index.php
@@ -31,4 +31,19 @@ $app->register(new Herrera\Pdo\PdoServiceProvider(),
                )
 );
 
+$app->get('/db/', function() use($app) {
+  $st = $app['pdo']->prepare('SELECT name FROM test_table');
+  $st->execute();
+
+  $names = array();
+  while ($row = $st->fetch(PDO::FETCH_ASSOC)) {
+    $app['monolog']->addDebug('Row ' . $row['name']);
+    $names[] = $row;
+  }
+
+  return $app['twig']->render('database.twig', array(
+    'names' => $names
+  ));
+});
+
 $app->run();
